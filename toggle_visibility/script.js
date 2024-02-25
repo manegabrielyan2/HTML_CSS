b57aa9eab2197f696b2ce1e6bc8f521bf4458eb7
manegabrielyan2 <136605577+manegabrielyan2@users.noreply.github.com>
2024-02-25 22:38:48 +0400

    Add files via upload

---
+++ toggle_visibility/script.js
@@ -0,0 +1,8 @@
+function toggleVisibility() {
+    var div = document.getElementById("myDiv");
+    if (div.style.visibility === "visible") {
+        div.style.visibility = "hidden";
+    } else {
+        div.style.visibility = "visible";
+    }
+}
