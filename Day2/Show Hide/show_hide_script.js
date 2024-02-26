9e67cb0490b40af2e931544e5c222f05c976063d
manegabrielyan2 <136605577+manegabrielyan2@users.noreply.github.com>
2024-02-26 22:41:31 +0400

    Update show_hide_script.js

--- Day2/Show Hide/show_hide_script.js
+++ Day2/Show Hide/show_hide_script.js
@@ -1,17 +1,14 @@
 function show_hide(){
+    var m = document.getElementById("hide");
     var n = document.getElementById("par");
-    if( n.style.visibility == "visible"){
+    if( m.textContent === "Hide"){
+        m.textContent = "Show";
         n.style.visibility = "hidden";
     }
     else{
+        m.textContent = "Hide";
         n.style.visibility = "visible";
     }
-    var m = document.getElementById("hide");
-    if( m.textContent === "Hide"){
-        m.textContent = "Show";
-    }
-    else{
-        m.textContent = "Hide";
-    }
+    
 
 } 
